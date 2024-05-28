914313ebf6ccde1289fcd22341c8adcb75761028
Дмитрий Каблучко <xequrt@yandex.ru>
2024-05-28 20:40:27 +1000

    Add options [-f] and description

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,5 +2,10 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help = 'set format of output')
     parser.parse_args()
