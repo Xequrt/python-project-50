58d2ab2b8dc0e2226ff146dd5ae8661c47edbb32
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-02 20:35:02 +1000

    Conversion of JSON syntax to a string

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,10 @@
 from gendiff.cli import parse_arguments
 from gendiff.generate_diff import generate_diff
-import json
 
 def main():
     args = parse_arguments()
     diff = generate_diff(args.first_file, args.second_file, args.format)
-    print(json.dumps(diff, indent=4))
+    print(diff)
 
     if __name__ == '__main__':
         main()
