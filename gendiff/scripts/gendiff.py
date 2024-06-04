32b6d4fd70dae1ad99ad5b7ec3b36fbb42446629
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-04 22:02:41 +1000

    FIX flake8

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 from gendiff.cli import parse_arguments
 from gendiff.generate_diff import generate_diff
 
+
 def main():
     args = parse_arguments()
     diff = generate_diff(args.first_file, args.second_file, args.format)
