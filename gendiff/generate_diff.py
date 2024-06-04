c89a394c0925b071b4ed81a925a3d063cce37b7b
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-04 22:02:26 +1000

    FIX flake8 C901 def generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,6 +2,20 @@ from gendiff.data import extensions_data
 from gendiff.parser import parse
 
 
+def format_line(key, value, prefix):
+    return f'{prefix} {key}: {str(value).lower()}\n'
+
+
+def compare_and_append(diff, key, parsed_data1, parsed_data2):
+    if parsed_data1[key] == parsed_data2[key]:
+        diff.append(format_line(key, parsed_data1[key], ' '))
+    else:
+        if key in parsed_data1:
+            diff.append(format_line(key, parsed_data1[key], '-'))
+        if key in parsed_data2:
+            diff.append(format_line(key, parsed_data2[key], '+'))
+
+
 def generate_diff(file1_path, file2_path, output_format):
     data1, extension1 = extensions_data(file1_path)
     data2, extension2 = extensions_data(file2_path)
@@ -9,18 +23,13 @@ def generate_diff(file1_path, file2_path, output_format):
     parsed_data1 = parse(data1, extension1)
     parsed_data2 = parse(data2, extension2)
     diff = []
+
     for key in sorted(set(parsed_data1.keys()) | set(parsed_data2.keys())):
         if key in parsed_data1 and key in parsed_data2:
-            if parsed_data1[key] == parsed_data2[key]:
-                diff.append(f'  {key}: {str(parsed_data1[key]).lower()}\n')
-            elif parsed_data1[key] != parsed_data2[key]:
-                if key in parsed_data1:
-                    diff.append(f'- {key}: {str(parsed_data1[key]).lower()}\n')
-                if key in parsed_data2:
-                    diff.append(f'+ {key}: {str(parsed_data2[key]).lower()}\n')
-
+            compare_and_append(diff, key, parsed_data1, parsed_data2)
         elif key in parsed_data1:
-            diff.append(f'- {key}: {str(parsed_data1[key]).lower()}\n')
+            diff.append(format_line(key, parsed_data1[key], '-'))
         else:
-            diff.append(f'+ {key}: {str(parsed_data2[key]).lower()}\n')
+            diff.append(format_line(key, parsed_data2[key], '+'))
+
     return "{\n  " + "  ".join(diff) + "}"
