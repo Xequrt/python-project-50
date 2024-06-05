4e3778ae693440aa7086adbbeaff6e428356f8c0
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-05 11:21:10 +1000

    Add test for YML, YAML files

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,7 +4,13 @@ from gendiff import generate_diff
 @pytest.mark.parametrize('file1, file2, result_file', [
     ('./tests/fixtures/file1.json',
      './tests/fixtures/file2.json',
-     './tests/fixtures/correct_result_json.txt')
+     './tests/fixtures/correct_result_json.txt'),
+    ('./tests/fixtures/file1.yml',
+     './tests/fixtures/file2.yml',
+     './tests/fixtures/correct_result_yml.txt'),
+    ('./tests/fixtures/file1.yaml',
+     './tests/fixtures/file2.yaml',
+     './tests/fixtures/correct_result_yaml.txt')
 ])
 def test_generate_diff(file1, file2, result_file):
     diff = generate_diff(file1, file2, output_format=format)
