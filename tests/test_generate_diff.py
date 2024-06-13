0a6a38613d1e45a8428d82761fba6283749329df
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-13 20:32:19 +1000

    Add tests tree yaml, yml, json

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -10,7 +10,16 @@ from gendiff import generate_diff
      './tests/fixtures/correct_result_yml.txt'),
     ('./tests/fixtures/file1.yaml',
      './tests/fixtures/file2.yaml',
-     './tests/fixtures/correct_result_yaml.txt')
+     './tests/fixtures/correct_result_yaml.txt'),
+    ('./tests/fixtures/file1_tree.json',
+     './tests/fixtures/file2_tree.json',
+     './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
+    ('./tests/fixtures/file1_tree.yml',
+     './tests/fixtures/file2_tree.yml',
+     './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
+    ('./tests/fixtures/file1_tree.yaml',
+     './tests/fixtures/file2_tree.yaml',
+     './tests/fixtures/correct_result_tree_json_yaml_yml.txt')
 ])
 def test_generate_diff(file1, file2, result_file):
     diff = generate_diff(file1, file2, output_format=format)
