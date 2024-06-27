f2c71bd02223f02840e327b95fa4f2278ec84ee0
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-27 20:47:10 +1000

    Fix tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,27 +1,33 @@
 import pytest
 from gendiff import generate_diff
 
-@pytest.mark.parametrize('file1, file2, result_file', [
+@pytest.mark.parametrize('file1, file2, format, result_file', [
     ('./tests/fixtures/file1.json',
      './tests/fixtures/file2.json',
+     'stylish',
      './tests/fixtures/correct_result_json.txt'),
     ('./tests/fixtures/file1.yml',
      './tests/fixtures/file2.yml',
+     'stylish',
      './tests/fixtures/correct_result_yml.txt'),
     ('./tests/fixtures/file1.yaml',
      './tests/fixtures/file2.yaml',
+     'stylish',
      './tests/fixtures/correct_result_yaml.txt'),
     ('./tests/fixtures/file1_tree.json',
      './tests/fixtures/file2_tree.json',
+     'stylish',
      './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
     ('./tests/fixtures/file1_tree.yml',
      './tests/fixtures/file2_tree.yml',
+     'stylish',
      './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
     ('./tests/fixtures/file1_tree.yaml',
      './tests/fixtures/file2_tree.yaml',
+     'stylish',
      './tests/fixtures/correct_result_tree_json_yaml_yml.txt')
 ])
-def test_generate_diff(file1, file2, result_file):
+def test_generate_diff(file1, file2, format, result_file):
     diff = generate_diff(file1, file2, output_format=format)
     with open(result_file) as result_file:
         result = result_file.read()
