b464d4bbaee2153039908088ffc51c79c15c5063
Дмитрий Каблучко <xequrt@yandex.ru>
2024-06-29 21:10:18 +1000

    FIX stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,4 +1,4 @@
-PREF = {
+PREFIX = {
     "added": '+ ',
     "removed": '- ',
     "unchanged": '  ',
@@ -7,14 +7,14 @@ PREF = {
 DEFAULT_INDENT = 4
 
 
-def to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
+def convert_to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
     if isinstance(value, dict):
         current_indent = indent_char * depth
         child_indent = indent_char * (depth + indent_size)
         lines = ["{"]
         for key, val in value.items():
             lines.append(f"{child_indent}{key}: "
-                         f"{to_str(val, indent_char, indent_size)}")
+                         f"{convert_to_str(val, indent_char, indent_size)}")
         lines.append(f"{current_indent}}}")
         return "\n".join(lines)
     elif isinstance(value, bool):
@@ -25,41 +25,36 @@ def to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
         return str(value)
 
 
-def stylish_children(diff, depth, indent_char, indent_size):
-    lines = list(
-        map(
-            lambda x: stylish(x, depth + 1, indent_char, indent_size),
-            diff['children']
-        )
-    )
-    result = '\n'.join(lines)
-    return result
-
-
 def stylish(diff, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
     child_indent = indent_char * (depth * indent_size - 2)
-    if diff['value'] in ('root', 'tree'):
-        result = stylish_children(diff, depth, indent_char, indent_size)
-        if diff['value'] == 'root':
-            return f'{{\n{result}\n}}'
-        elif diff['value'] == 'tree':
-            return (f"{child_indent}{PREF['tree']}{diff['key']}:"
-                    f" {{\n{result}\n{child_indent}}}")
-    handlers = {
-        'added': lambda: f"{child_indent}{PREF['added']}{diff['key']}: "
-                         f"{to_str(diff['old'], depth)}",
-        'removed': lambda: f"{child_indent}{PREF['removed']}{diff['key']}: "
-                           f"{to_str(diff['new'], depth)}",
-        'unchanged': lambda: f"{child_indent}{PREF['unchanged']}{diff['key']}: "
-                             f"{to_str(diff['meta'], depth)}",
-        'changed': lambda: (f"{child_indent}{PREF['removed']}{diff['key']}: "
-                            f"{to_str(diff['new'], depth)}\n"
-                            f"{child_indent}{PREF['added']}{diff['key']}: "
-                            f"{to_str(diff['old'], depth)}"),
-    }
-
-    handler = handlers.get(diff['value'])
-    if handler:
-        return handler()
-    else:
-        raise ValueError(f"Unknown diff value: {diff['value']}")
+    if diff['value'] == 'root':
+        lines = list(
+            map(
+                lambda x: stylish(x, depth + 1, indent_char, indent_size),
+                diff['children']
+            )
+        )
+        result = '\n'.join(lines)
+        return f'{{\n{result}\n}}'
+    elif diff['value'] == 'tree':
+        lines = list(
+            map(
+                lambda x: stylish(x, depth + 1, indent_char, indent_size),
+                diff['children']))
+        result = '\n'.join(lines)
+        return (f"{child_indent}{PREFIX['tree']}{diff['key']}:"
+                f" {{\n{result}\n{child_indent}}}")
+    elif diff['value'] == 'added':
+        return (f"{child_indent}{PREFIX['added']}{diff['key']}:"
+                f" {convert_to_str(diff['old'], depth)}")
+    elif diff['value'] == 'removed':
+        return (f"{child_indent}{PREFIX['removed']}{diff['key']}:"
+                f" {convert_to_str(diff['new'], depth)}")
+    elif diff['value'] == 'unchanged':
+        return (f"{child_indent}{PREFIX['unchanged']}{diff['key']}:"
+                f" {convert_to_str(diff['meta'], depth)}")
+    elif diff['value'] == 'changed':
+        return (f"{child_indent}{PREFIX['removed']}{diff['key']}:"
+                f" {convert_to_str(diff['new'], depth)}\n"
+                f"{child_indent}{PREFIX['added']}{diff['key']}:"
+                f" {convert_to_str(diff['old'], depth)}")
