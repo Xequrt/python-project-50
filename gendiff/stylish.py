f33350307a4772c897354481fe4334980d66b137
Дмитрий Каблучко <xequrt@yandex.ru>
2024-07-09 17:09:15 +1000

    FIX stylish

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -9,13 +9,15 @@ DEFAULT_INDENT = 4
 
 def convert_to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
     if isinstance(value, dict):
-        current_indent = indent_char * depth
-        child_indent = indent_char * (depth + indent_size)
+        child_indent = get_indent('  ', depth + 1)
         lines = ["{"]
         for key, val in value.items():
-            lines.append(f"{child_indent}{key}: "
-                         f"{convert_to_str(val, depth + 1, indent_char, indent_size)}")
-        lines.append(f"{current_indent}}}")
+            if isinstance(val, dict):
+                lines.append(f"{child_indent}{key}: "
+                             f"{convert_to_str(val, depth + 1, indent_char, indent_size)}")
+            else:
+                lines.append(f'{child_indent}{key}: {convert_to_str(val, depth, indent_char, indent_size)}')
+        lines.append(f"{get_indent('   ', depth)}}}")
         return "\n".join(lines)
     elif isinstance(value, bool):
         return str(value).lower()
@@ -25,12 +27,17 @@ def convert_to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
         return str(value)
 
 
+def get_indent(sign, depth, indent_char=' ', indent_size=DEFAULT_INDENT):
+    indent_size = (depth * indent_size - 2)
+    indent = indent_char * indent_size + PREFIX.get(sign, sign)
+    return indent
+
+
 def stylish(diff, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
-    child_indent = indent_char * (depth * indent_size - 2)
     if diff['type'] == 'root':
         lines = list(
             map(
-                lambda x: stylish(x, depth + 1, indent_char, indent_size),
+                lambda x: stylish(x, depth + 1),
                 diff['children']
             )
         )
@@ -39,22 +46,22 @@ def stylish(diff, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
     elif diff['type'] == 'tree':
         lines = list(
             map(
-                lambda x: stylish(x, depth + 1, indent_char, indent_size),
+                lambda x: stylish(x, depth + 1),
                 diff['children']))
         result = '\n'.join(lines)
-        return (f"{child_indent}{PREFIX['tree']}{diff['key']}:"
-                f" {{\n{result}\n{child_indent}}}")
+        return (f"{get_indent(PREFIX['tree'], depth)}{diff['key']}:"
+                f" {{\n{result}\n{get_indent(PREFIX['tree'], depth)}}}")
     elif diff['type'] == 'added':
-        return (f"{child_indent}{PREFIX['added']}{diff['key']}:"
+        return (f"{get_indent(PREFIX['added'], depth)}{diff['key']}:"
                 f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
     elif diff['type'] == 'removed':
-        return (f"{child_indent}{PREFIX['removed']}{diff['key']}:"
+        return (f"{get_indent(PREFIX['removed'], depth)}{diff['key']}:"
                 f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
     elif diff['type'] == 'unchanged':
-        return (f"{child_indent}{PREFIX['unchanged']}{diff['key']}:"
+        return (f"{get_indent(PREFIX['unchanged'], depth)}{diff['key']}:"
                 f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
     elif diff['type'] == 'changed':
-        return (f"{child_indent}{PREFIX['removed']}{diff['key']}:"
+        return (f"{get_indent(PREFIX['removed'], depth)}{diff['key']}:"
                 f" {convert_to_str(diff['old'], depth, indent_char, indent_size)}\n"
-                f"{child_indent}{PREFIX['added']}{diff['key']}:"
+                f"{get_indent(PREFIX['added'], depth)}{diff['key']}:"
                 f" {convert_to_str(diff['new'], depth, indent_char, indent_size)}")
