be62b19dcd87b360cfcd5fe0c195d84087f17350
Дмитрий Каблучко <xequrt@yandex.ru>
2024-07-11 15:02:56 +1000

    FIX stylish output

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -7,18 +7,19 @@ PREFIX = {
 DEFAULT_INDENT = 4
 
 
-def convert_to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
+def convert_to_str(value, depth=0, indent_char=' '):
+    indent_size = (indent_char * (DEFAULT_INDENT - 2))
     if isinstance(value, dict):
-        child_indent = get_indent('  ', depth + 1)
+        child_indent = get_indent(depth + 1)
         lines = ["{"]
         for key, val in value.items():
             if isinstance(val, dict):
-                lines.append(f"{child_indent}{key}: "
-                             f"{convert_to_str(val, depth + 1, indent_char, indent_size)}")
+                lines.append(f"{indent_size}{child_indent}{key}: "
+                             f"{convert_to_str(val, depth + 1)}")
             else:
-                lines.append(f'{child_indent}{key}: '
-                             f'{convert_to_str(val, depth, indent_char, indent_size)}')
-        lines.append(f"{get_indent('  ', depth)}}}")
+                lines.append(f'{indent_size}{child_indent}{key}: '
+                             f'{convert_to_str(val, depth)}')
+        lines.append(f"{get_indent(depth)}{indent_size}}}")
         return "\n".join(lines)
     elif isinstance(value, bool):
         return str(value).lower()
@@ -28,13 +29,13 @@ def convert_to_str(value, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
         return str(value)
 
 
-def get_indent(sign, depth, indent_char=' ', indent_size=DEFAULT_INDENT):
+def get_indent(depth, indent_char=' ', indent_size=DEFAULT_INDENT):
     indent_size = (depth * indent_size - 2)
-    indent = indent_char * indent_size + PREFIX.get(sign, sign)
+    indent = indent_char * indent_size
     return indent
 
 
-def stylish(diff, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
+def stylish(diff, depth=0):
     if diff['type'] == 'root':
         lines = list(
             map(
@@ -50,19 +51,19 @@ def stylish(diff, depth=0, indent_char=' ', indent_size=DEFAULT_INDENT):
                 lambda x: stylish(x, depth + 1),
                 diff['children']))
         result = '\n'.join(lines)
-        return (f"{get_indent(PREFIX['tree'], depth)}{diff['key']}:"
-                f" {{\n{result}\n{get_indent(PREFIX['tree'], depth)}}}")
+        return (f"{get_indent(depth)}{PREFIX['tree']}{diff['key']}:"
+                f" {{\n{result}\n{get_indent(depth)}{PREFIX['tree']}}}")
     elif diff['type'] == 'added':
-        return (f"{get_indent(PREFIX['added'], depth)}{diff['key']}:"
-                f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
+        return (f"{get_indent(depth)}{PREFIX['added']}{diff['key']}:"
+                f" {convert_to_str(diff['value'], depth)}")
     elif diff['type'] == 'removed':
-        return (f"{get_indent(PREFIX['removed'], depth)}{diff['key']}:"
-                f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
+        return (f"{get_indent(depth)}{PREFIX['removed']}{diff['key']}:"
+                f" {convert_to_str(diff['value'], depth)}")
     elif diff['type'] == 'unchanged':
-        return (f"{get_indent(PREFIX['unchanged'], depth)}{diff['key']}:"
-                f" {convert_to_str(diff['value'], depth, indent_char, indent_size)}")
+        return (f"{get_indent(depth)}{PREFIX['unchanged']}{diff['key']}:"
+                f" {convert_to_str(diff['value'], depth)}")
     elif diff['type'] == 'changed':
-        return (f"{get_indent(PREFIX['removed'], depth)}{diff['key']}:"
-                f" {convert_to_str(diff['old'], depth, indent_char, indent_size)}\n"
-                f"{get_indent(PREFIX['added'], depth)}{diff['key']}:"
-                f" {convert_to_str(diff['new'], depth, indent_char, indent_size)}")
+        return (f"{get_indent(depth)}{PREFIX['removed']}{diff['key']}:"
+                f" {convert_to_str(diff['old'], depth)}\n"
+                f"{get_indent(depth)}{PREFIX['added']}{diff['key']}:"
+                f" {convert_to_str(diff['new'], depth)}")
