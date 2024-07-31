a02e2a579b7d5d391fa6f0f7542e799d7a231311
Дмитрий Каблучко <xequrt@yandex.ru>
2024-07-31 23:32:42 +1000

    fix review #1, tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,43 +1,51 @@
+import os
 import pytest
 from gendiff import generate_diff
 
 
+def get_absolute_path(filename):
+    return os.path.join(os.path.dirname(__file__), 'fixtures', filename)
+
+
 @pytest.mark.parametrize('file1, file2, format, result_file', [
-    ('./tests/fixtures/file1.json',
-     './tests/fixtures/file2.json',
+    ('file1.json',
+     'file2.json',
      'stylish',
-     './tests/fixtures/correct_result_json.txt'),
-    ('./tests/fixtures/file1.yml',
-     './tests/fixtures/file2.yml',
+     'correct_result_json.txt'),
+    ('file1.yml',
+     'file2.yml',
      'stylish',
-     './tests/fixtures/correct_result_yml.txt'),
-    ('./tests/fixtures/file1.yaml',
-     './tests/fixtures/file2.yaml',
+     'correct_result_yml.txt'),
+    ('file1.yaml',
+     'file2.yaml',
      'stylish',
-     './tests/fixtures/correct_result_yaml.txt'),
-    ('./tests/fixtures/file1_tree.json',
-     './tests/fixtures/file2_tree.json',
+     'correct_result_yaml.txt'),
+    ('file1_tree.json',
+     'file2_tree.json',
      'stylish',
-     './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
-    ('./tests/fixtures/file1_tree.yml',
-     './tests/fixtures/file2_tree.yml',
+     'correct_result_tree_json_yaml_yml.txt'),
+    ('file1_tree.yml',
+     'file2_tree.yml',
      'stylish',
-     './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
-    ('./tests/fixtures/file1_tree.yaml',
-     './tests/fixtures/file2_tree.yaml',
+     'correct_result_tree_json_yaml_yml.txt'),
+    ('file1_tree.yaml',
+     'file2_tree.yaml',
      'stylish',
-     './tests/fixtures/correct_result_tree_json_yaml_yml.txt'),
-    ('./tests/fixtures/file1_tree.json',
-     './tests/fixtures/file2_tree.json',
+     'correct_result_tree_json_yaml_yml.txt'),
+    ('file1_tree.json',
+     'file2_tree.json',
      'plain',
-     './tests/fixtures/correct_result_plain.txt'),
-    ('./tests/fixtures/file1_tree.json',
-     './tests/fixtures/file2_tree.json',
+     'correct_result_plain.txt'),
+    ('file1_tree.json',
+     'file2_tree.json',
      'json',
-     './tests/fixtures/correct_result_json_format.txt')
+     'correct_result_json_format.txt')
 ])
 def test_generate_diff(file1, file2, format, result_file):
-    diff = generate_diff(file1, file2, output_format=format)
-    with open(result_file) as result_file:
+    file1_path = get_absolute_path(file1)
+    file2_path = get_absolute_path(file2)
+    result_file_path = get_absolute_path(result_file)
+    diff = generate_diff(file1_path, file2_path, output_format=format)
+    with open(result_file_path) as result_file:
         result = result_file.read()
         assert result == diff
